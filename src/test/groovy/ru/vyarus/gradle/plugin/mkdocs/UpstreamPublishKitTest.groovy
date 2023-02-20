0b5e3d41befc2f525d04c6b15538d3fd037f2741
Vyacheslav Rusakov <vyarus@gmail.com>
2023-02-20 14:26:46 +0700

    update upstream test to gradle 8

--- src/test/groovy/ru/vyarus/gradle/plugin/mkdocs/UpstreamPublishKitTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/plugin/mkdocs/UpstreamPublishKitTest.groovy
@@ -11,7 +11,7 @@ import spock.lang.TempDir
  */
 class UpstreamPublishKitTest extends AbstractKitTest {
 
-    String GRADLE_VERSION = '7.5'
+    String GRADLE_VERSION = '8.0'
 
     @TempDir File repoDir
 
